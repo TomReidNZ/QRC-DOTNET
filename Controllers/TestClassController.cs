b83cc124a514ce3c9af50615ca64c4c9505c19ba
tomreidnz <thomasreid@Thomass-MacBook-Pro.local>
2023-05-24 14:07:19 -0300

    add new test class and route

---
+++ Controllers/TestClassController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace my_app.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestClassController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<TestClassController> _logger;
+
+    public TestClassController(ILogger<TestClassController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<TestClass> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new TestClass
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
